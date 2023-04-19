fbacb5062132b1c2ed8f6b18c3a62f4811001047
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2023-04-19 23:00:32 +0300

    remove unused imports

--- frontend/smart_tutor/src/pages/index.tsx
+++ frontend/smart_tutor/src/pages/index.tsx
@@ -1,13 +1,10 @@
 //@ts-nocheck
 // import haro section
-import { Container } from "@chakra-ui/react";
 import Hero_section from "../components/landing_page/hero_section";
 import Find_tutor_section from "../components/landing_page/find_tutor_section";
 import Become_tutor_section from "../components/landing_page/become_tutor_section";
-import Partners_section from "../components/landing_page/partners_section";
 import Our_mission_section from "../components/landing_page/our_mission_section/our_mission_section";
 import TopRatedTutorsSection from "../components/landing_page/top_rated_tutors/top_rated_tutors_section";
-import All_request_cards from "../components/Cards/RequestCard/all_request_cards";
 
 import { Button } from "@chakra-ui/react";
 export default function Home() {
